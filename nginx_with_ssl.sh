a680cec2bd0e745e2e5b87b72f68960909f046c4
Emil <emil@esnx.xyz>
2023-03-14 17:51:06 +0100

    Escape false variable

--- nginx_with_ssl.sh
+++ nginx_with_ssl.sh
@@ -49,7 +49,7 @@ server {
 
 	location / {
         autoindex off;
-        try_files $uri $uri/ =403;
+        try_files \$uri \$uri/ =403;
 	}
 }
 EOF
@@ -64,6 +64,3 @@ systemctl restart nginx
 certbot --nginx -d $DOMAIN -d www.$DOMAIN
 systemctl status snap.certbot.renew.service  
 certbot renew --dry-run
-
-# quick fix certbot nginx config
-sed -i 's/try_files  \/ =403;/try_files \$uri \$uri\/ =403;/g' /etc/nginx/sites-enabled/$DOMAIN

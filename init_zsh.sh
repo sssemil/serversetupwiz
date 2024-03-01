665a1abe80f78dcaaa738ce05e863a30deaf7870
Emil <emil@esnx.xyz>
2024-03-01 07:27:53 +0000

    Update init_zsh.sh

--- init_zsh.sh
+++ init_zsh.sh
@@ -8,13 +8,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cat <<EOF > ~/.zshrc
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-export ZSH="$HOME/.oh-my-zsh"
+export PATH=\$HOME/bin:/usr/local/bin:\$PATH
+export ZSH="\$HOME/.oh-my-zsh"
 ZSH_THEME="agnoster"
 plugins=(
     git
     zsh-autosuggestions
     zsh-syntax-highlighting
 )
-source $ZSH/oh-my-zsh.sh
+source \$ZSH/oh-my-zsh.sh
 EOF

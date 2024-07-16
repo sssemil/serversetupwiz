753edac4e2690de840f60b780e436669ec2560fe
Emil <emil@esnx.xyz>
2024-07-16 09:32:29 +0000

    Unattended oh-my-zsh install

--- init_zsh.sh
+++ init_zsh.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
